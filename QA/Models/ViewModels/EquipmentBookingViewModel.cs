663738185a1151ffb08793a30013e2852bc2132d
Momelezi <momeleziqasana97@gmail.com>
2022-09-09 13:46:41 +0200

    add ViewModel

---
+++ QA/Models/ViewModels/EquipmentBookingViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace QA.Models.ViewModels
+{
+    public class EquipmentBookingViewModel
+    {
+        public int EquipmentId { get; set; }
+        public int BookingId { get; set; }
+
+        public BookingViewModel Booking { get; set; }
+        public EquipmentViewModel Equipment { get; set; }
+    }
+}
---
+++ QA/Models/ViewModels/EquipmentViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace QA.Models.ViewModels
+{
+    public class EquipmentViewModel
+    {
+        private ICollection<EquipmentBooking> _equipmentBookings;
+        //public int EquipmentId { get; set; }
+        public string Descr { get; set; }
+        public int EquipmentCategoryId { get; set; }
+        public virtual ICollection<EquipmentBooking> EquipmentBookings
+        {
+            get => _equipmentBookings ?? (_equipmentBookings = new List<EquipmentBooking>());
+            set => _equipmentBookings = value;
+        }
+    }
+}
